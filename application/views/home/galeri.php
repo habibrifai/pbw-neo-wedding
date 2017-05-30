79c0a612d291c5852f8e46a8c3bae288494c8e1d
DESKTOP-BSH050T\who <rifaihabib29@gmail.com>
2017-05-30 14:59:37 +0700

    email

--- application/views/home/galeri.php
+++ application/views/home/galeri.php
@@ -39,7 +39,7 @@
 		<section class=galeri>
 			<div class="container">
         <h1 class="text-center uppercase"><br><strong>Galeri</strong></h1>
-        <div class="row ">
+        <di v class="row ">
         <?php foreach($gallery as $b){ ?>
 	          <div class="col-xs-12 col-sm-6 col-md-4">
 							<div class="img-wrapper animated fadeInUp">
